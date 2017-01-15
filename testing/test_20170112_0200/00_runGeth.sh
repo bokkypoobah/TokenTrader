127d50051e492146ca009e2c3d3a02763ae44783
BokkyPooBah <bokky.poobah@bokconsulting.com.au>
2017-01-16 10:08:45 +1100

    Delete 00_runGeth.sh

--- testing/test_20170112_0200/00_runGeth.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-rm -f ./testchain/geth/chaindata/*
-
-geth --datadir ./testchain init genesis.json
-
-geth --datadir ./testchain --unlock 0 --password ./testpassword --rpc --rpccorsdomain '*' --rpcport 8646 --port 32323 --mine --minerthreads 1 --maxpeers 0 console
-
